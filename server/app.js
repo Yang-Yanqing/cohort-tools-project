23f1f7d46f4957655301e79c530e193f3d48a9c0
Yanqing Yang <yang.yanqing@tutamail.com>
2025-10-13 16:13:03 +0200

    10.13 Finished

--- server/app.js
+++ server/app.js
@@ -2,10 +2,13 @@ const express = require("express");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 const PORT = 5005;
-
+const students=require("./students.json")
+const cohorts=require("./cohorts.json")
+const cors=require('cors')
 // STATIC DATA
 // Devs Team - Import the provided files with JSON data of students and cohorts here:
 // ...
+
 
 
 // INITIALIZE EXPRESS APP - https://expressjs.com/en/4x/api.html#express
@@ -16,21 +19,25 @@ const app = express();
 // Research Team - Set up CORS middleware here:
 // ...
 app.use(express.json());
+app.use(cors());
 app.use(morgan("dev"));
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 
+
 // ROUTES - https://expressjs.com/en/starter/basic-routing.html
 // Devs Team - Start working on the routes here:
 // ...
 app.get("/docs", (req, res) => {
-  res.sendFile(__dirname + "/views/docs.html");
+  res.sendFile(__dirname+"/views/docs.html")
 });
 
+app.get("/api/cohorts",(req,res)=>{res.json(cohorts)});
+app.get("/api/students",(req,res)=>{res.json(students)});
 
 // START SERVER
-app.listen(PORT, () => {
+app.listen(5005, () => {
   console.log(`Server listening on port ${PORT}`);
 });
